a56ddfd7bc2308e0eaab35d09c79e113169d1cc9
shafali03 <shaf03sm@gmail.com>
2021-03-03 22:19:20 +0000

    Updated the Product component and added styles

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,26 +1,35 @@
 import React from 'react'
-import Grid from '@material-ui/core'
+import { Grid } from '@material-ui/core'
+
+import Product from './Product/Product'
 
 const products = [
-  { id: 1, name: 'HUGO', description: 'Deep Red Eau de Parfum', price: '£35' },
-  { id: 2, name: 'Jean Paul Gaultier', description: 'Classique by Jean Paul Gaultier Eau De Toilette', price: '£59' },
+  {
+    id: 1, name: 'COCO', description: 'Chanel', price: '£135',
+    image: 'https://res.cloudinary.com/shafali/image/upload/v1599648436/chanel-coco_pv73fc.jpg'
+  },
+  { id: 2, name: 'Jean Paul Gaultier', description: 'Jean Paul Gaultier Eau De Toilette', price: '£59', image: 'https://res.cloudinary.com/shafali/image/upload/v1599648203/Jean-paul_jiebmo.jpg' },
 ]
 
 const Products = () => {
-  <main>
-    <Grid container justify='center' spacing={4}>
-      {products.map((product) => (
-        <Grid item key={product.id}
-          xs={12}
-          xs={12}
-          sm={6}
-          md={4}
-          lg={3}>
-          <Product product={product} />
-        </Grid>
-      ))}
-    </Grid>
-  </main>
+
+  return (
+    <main>
+      <Grid container justify='center' spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id}
+            xs={12}
+            sm={6}
+            md={4}
+            lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+
+  )
+
 }
 
 export default Products
