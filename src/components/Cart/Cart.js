256bc44fe81fe760ca38f234819bf3e3c92fc6d6
shafali03 <shaf03sm@gmail.com>
2021-03-13 18:13:12 +0000

    Displaying cart items in the cart and added remove button for the items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
+
 import useStyles from './styles'
+import CartItem from '../CartItem/CartItem'
 
 const Cart = ({ cart }) => {
   
@@ -15,7 +17,7 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            <div>{item.name}</div>
+           <CartItem item={item}/>
           </Grid>
         ))}
       </Grid>
@@ -37,7 +39,7 @@ const Cart = ({ cart }) => {
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title} variant='h3'>Your Shopping Cart</Typography>
+      <Typography className={classes.title} variant='h3' gutterBottom>Your Shopping Cart</Typography>
       { cart.line_items.length === 0 ? <EmptyCart /> : <FilledCart />}
     </Container>
   )
