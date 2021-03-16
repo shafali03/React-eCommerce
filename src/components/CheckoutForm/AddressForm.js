0c9456d535a27a22977a2511ecceb33e2ddf6ed8
shafali03 <shaf03sm@gmail.com>
2021-03-16 09:13:48 +0000

    Rendering activeStep for checkout addressForm, PaymentForm and confirmation

---
+++ src/components/CheckoutForm/AddressForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const AddressForm = () => {
+  return (
+    <div>
+      AddressForm
+    </div>
+  )
+}
+
+export default AddressForm
--- src/components/CheckoutForm/Checkout/Checkout.js
+++ src/components/CheckoutForm/Checkout/Checkout.js
@@ -1,12 +1,25 @@
 import React, { useState } from 'react'
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core'
 import useStyles from './styles'
+import AddressForm from '../AddressForm'
+import PaymentForm from '../PaymentForm'
 
 const steps = ['Shipping address', 'Payment details']
 
 const Checkout = () => {
-const [activeStep, setActiveStep] = useState(0)
+const [activeStep, setActiveStep] = useState(2)
 const classes = useStyles()
+
+const Confirmation = () => (
+  <div>
+    Confirmation
+  </div>
+)
+
+
+const Form = () => activeStep === 0
+? <AddressForm />
+: <PaymentForm />
 
   return (
     <>
@@ -21,6 +34,7 @@ const classes = useStyles()
               </Step>
             ))}
           </Stepper>
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
         </Paper>
       </main> 
     </>
