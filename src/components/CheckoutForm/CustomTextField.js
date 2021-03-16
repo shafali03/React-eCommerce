7dd2ca60aa035aaba496d7b3c71107ff822b96ab
shafali03 <shaf03sm@gmail.com>
2021-03-16 09:50:08 +0000

    Created a shipping address form

--- src/components/CheckoutForm/CustomTextField.js
+++ src/components/CheckoutForm/CustomTextField.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {TextField, Grid} from '@material-ui/core'
 import {useFormContext, Controller} from 'react-hook-form'
 
-const CustomTextField = ({name, label, required}) => {
+const FormField = ({name, label, required}) => {
   const {control}= useFormContext()
   return (
     <Grid item xs={12} sm={6}>
@@ -18,4 +18,4 @@ const CustomTextField = ({name, label, required}) => {
   )
 }
 
-export default CustomTextField
+export default FormField
