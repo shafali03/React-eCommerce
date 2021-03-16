d02adead231deae6dbf229b0cd66997d7df4b6c7
shafali03 <shaf03sm@gmail.com>
2021-03-16 15:59:57 +0000

    Created select field for the country 

--- src/components/CheckoutForm/AddressForm.js
+++ src/components/CheckoutForm/AddressForm.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {InputLabel, Select, MenuItem, Button, Grid, Typography} from '@material-ui/core'
 import {useForm, FormProvider} from 'react-hook-form'
 import FormInput from './CustomTextField'
 
-const AddressForm = () => {
+import {commerce} from '../../lib/commerce'
+
+const AddressForm = ({checkoutToken}) => {
+  const [shippingCountries, setShippingCountries] = useState([])
+  const [shippingCountry, setShippingCountry] = useState('')
+  const [shippingSubdivisions, setShippingSubdivisions] = useState([])
+  const [shippingSubdivision, setShippingSubdivision] = useState('')
+  const [shippingOptions, setShippingOptions] = useState([])
+  const [shippingOption, setShippingOption] = useState('')
 const methods = useForm()
+
+  const countries = Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name }))
+
+const fetchShippingCountries = async (checkoutTokenId) => {
+  const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId)
+
+  console.log(countries)
+  setShippingCountries(countries)
+  setShippingCountry(Object.keys(countries)[0])
+}
+
+useEffect(() => {
+  fetchShippingCountries(checkoutToken.id)
+}, [])
+
 
   return (
     <>
@@ -18,6 +41,33 @@ const methods = useForm()
             <FormInput required name='email' label='Email' />
             <FormInput required name='city' label='City' />
             <FormInput required name='zip' label='Postcode' />
+
+            <Grid item xs={12} sm={6}>
+              <InputLabel>Shipping Country</InputLabel>
+              <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+             {countries.map((country) => (
+                <MenuItem key={country.id} value={country.id}>
+                 {country.label}
+                </MenuItem>
+             ))}
+              </Select>
+            </Grid>
+            {/* <Grid item xs={12} sm={6}>
+              <inputLabel>Shipping Subdivision</inputLabel>
+              <Select value={ } fullWidth onChange={ }>
+                <MenuItem key={ } value={ }>
+                  Select me
+                </MenuItem>
+              </Select>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <inputLabel>Shipping Options</inputLabel>
+              <Select value={ } fullWidth onChange={ }>
+                <MenuItem key={ } value={ }>
+                  Select me
+                </MenuItem>
+              </Select>
+            </Grid> */}
 
           </Grid>
         </form>
